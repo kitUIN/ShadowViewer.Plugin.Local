35325b9190bcd78357711e8fd0b4a11990b2497f
kitUIN <kulujun@gmail.com>
2025-02-07 16:52:04 +0800

    :sparkles: Update | 自动生成设置项

--- ShadowViewer.Plugin.Local/Enums/LocalSettingKey.cs
+++ ShadowViewer.Plugin.Local/Enums/LocalSettingKey.cs
@@ -1,29 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShadowPluginLoader.MetaAttributes;
 
-namespace ShadowViewer.Plugin.Local.Enums
+namespace ShadowViewer.Plugin.Local.Enums;
+
+/// <summary>
+/// 设置Key
+/// </summary>
+[ShadowSettingClass("ShadowViewer.Plugin.Local","LocalSettings")]
+[ShadowPluginSettingClass(typeof(LocalPlugin))]
+public enum LocalSettingKey
 {
-    public enum LocalSettingKey
-    {
-        /// <summary>
-        /// 允许相同文件夹导入
-        /// </summary>
-        LocalIsImportAgain,
-        LocalIsBookShelfInfoBar,
-        /// <summary>
-        /// 删除漫画同时删除漫画缓存
-        /// </summary>
-        LocalIsDeleteFilesWithComicDelete,
-        /// <summary>
-        /// 删除二次确认
-        /// </summary>
-        LocalIsRememberDeleteFilesWithComicDelete,
-        /// <summary>
-        /// 书架-样式-详细/简约
-        /// </summary>
-        LocalBookStyleDetail,
-    }
+    /// <summary>
+    /// 允许相同文件夹导入
+    /// </summary>
+    [ShadowSetting(typeof(bool),"false", "允许相同文件夹导入")]
+    LocalIsImportAgain,
+    /// <summary>
+    /// 显示书架左下角信息栏
+    /// </summary>
+    [ShadowSetting(typeof(bool), "true", "显示书架左下角信息栏")]
+    LocalIsBookShelfInfoBar,
+    /// <summary>
+    /// 删除漫画同时删除漫画缓存
+    /// </summary>
+    [ShadowSetting(typeof(bool), "false", "删除漫画同时删除漫画缓存")]
+    LocalIsDeleteFilesWithComicDelete,
+    /// <summary>
+    /// 删除二次确认
+    /// </summary>
+    [ShadowSetting(typeof(bool), "false", "删除二次确认")]
+    LocalIsRememberDeleteFilesWithComicDelete,
+    /// <summary>
+    /// 书架-样式-详细/简约
+    /// </summary>
+    [ShadowSetting(typeof(bool), "false", "书架-样式-详细/简约")] 
+    LocalBookStyleDetail,
 }
