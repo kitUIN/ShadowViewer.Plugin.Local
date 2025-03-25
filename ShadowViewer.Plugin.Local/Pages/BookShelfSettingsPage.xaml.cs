a531789567528fbf28374a752bbb5b9bf97e58ae
kitUIN <kulujun@gmail.com>
2025-03-25 18:04:41 +0800

    Fix | EntryPoint

--- ShadowViewer.Plugin.Local/Pages/BookShelfSettingsPage.xaml.cs
+++ ShadowViewer.Plugin.Local/Pages/BookShelfSettingsPage.xaml.cs
@@ -1,10 +1,11 @@
-using DryIoc;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Navigation; 
+using Microsoft.UI.Xaml.Navigation;
+using ShadowPluginLoader.Attributes;
 using ShadowViewer.Plugin.Local.ViewModels; 
 
 namespace ShadowViewer.Plugin.Local.Pages;
 
+[EntryPoint(Name = "SettingsPage")]
 public sealed partial class BookShelfSettingsPage : Page
 {
     private BookShelfSettingsViewModel ViewModel { get; } = new BookShelfSettingsViewModel();
