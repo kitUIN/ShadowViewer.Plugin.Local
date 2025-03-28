fb8cbea00994add71960019cd4ded0974b069b16
kituin <kit_uin@outlook.com>
2025-03-28 13:23:48 +0800

    Feat | EnumMenuFlyout

--- ShadowViewer.Plugin.Local/Enums/LocalReadMode.cs
+++ ShadowViewer.Plugin.Local/Enums/LocalReadMode.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using ShadowViewer.Controls.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentIcons.Common;
 
 namespace ShadowViewer.Plugin.Local.Enums;
 
@@ -14,10 +16,12 @@ public enum LocalReadMode
     /// <summary>
     /// 双页模式
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.BookOpen)]
     TwoPage,
 
     /// <summary>
     /// 滚动模式
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.DualScreenVerticalScroll)]
     Scrolling,
 }
--- ShadowViewer.Plugin.Local/Enums/LocalSort.cs
+++ ShadowViewer.Plugin.Local/Enums/LocalSort.cs
@@ -1,4 +1,7 @@
-﻿namespace ShadowViewer.Plugin.Local.Enums;
+using FluentIcons.Common;
+using ShadowViewer.Controls.Attributes;
+
+namespace ShadowViewer.Plugin.Local.Enums;
 
 /// <summary>
 /// 排序
@@ -8,33 +11,41 @@ public enum LocalSort
     /// <summary>
     /// 阅读进度小-大
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.BookOpen, IconVariant = IconVariant.Filled)]
     Pa,
     /// <summary>
     /// 阅读进度大-小
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.BookOpen)]
     Pz,
     /// <summary>
     /// 字母顺序A-Z
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.TextSortAscending)]
     Az,
     /// <summary>
     /// 字母顺序Z-A
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.TextSortDescending)]
     Za,
     /// <summary>
     /// 阅读时间早-晚
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.Clock, IconVariant = IconVariant.Filled)]
     Ra,
     /// <summary>
     /// 阅读时间晚-早(默认)
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.Clock)]
     Rz,
     /// <summary>
     /// 创建时间早-晚
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.Calendar, IconVariant = IconVariant.Filled)]
     Ca,
     /// <summary>
     /// 创建时间晚-早
     /// </summary>
+    [MenuFlyoutItemIcon(Icon = Icon.Calendar)]
     Cz
 }
