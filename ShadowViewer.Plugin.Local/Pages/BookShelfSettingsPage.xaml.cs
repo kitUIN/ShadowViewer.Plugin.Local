1c2407762324865290d7736049b9ccd4099e82f9
kituin <kit_uin@outlook.com>
2025-04-08 15:59:55 +0800

    Fix | 设置页面

--- ShadowViewer.Plugin.Local/Pages/BookShelfSettingsPage.xaml.cs
+++ ShadowViewer.Plugin.Local/Pages/BookShelfSettingsPage.xaml.cs
@@ -1,24 +1,39 @@
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using ShadowPluginLoader.Attributes;
-using ShadowViewer.Plugin.Local.ViewModels; 
+using ShadowViewer.Core.Plugins;
+using ShadowViewer.Plugin.Local.ViewModels;
 
 namespace ShadowViewer.Plugin.Local.Pages;
 
-[EntryPoint(Name = "SettingsPage")]
+/// <summary>
+/// 
+/// </summary>
+[EntryPoint(Name = nameof(PluginManage.SettingsPage))]
 public sealed partial class BookShelfSettingsPage : Page
 {
     private BookShelfSettingsViewModel ViewModel { get; } = new BookShelfSettingsViewModel();
 
+    /// <summary>
+    /// 
+    /// </summary>
     public BookShelfSettingsPage()
     {
         InitializeComponent();
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="e"></param>
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="e"></param>
     protected override void OnNavigatedFrom(NavigationEventArgs e)
     {
     }
